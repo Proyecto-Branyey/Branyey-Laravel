98c0b30713249ced22b315e9d22de3aada8e152b
yeison <davidyeison22@gmail.com>
2026-03-01 22:22:39 -0500

    fix: migraciones nuevas en carpeta correcta

---
+++ database/migrations/2026_03_02_030740_variante_color.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('variante_color', function (Blueprint $table) {
+            $table->id();
+            $table->integer('variante_id')->unsigned();
+            $table->integer('color_id')->unsigned();
+            $table->integer('orden')->default(1);
+            
+            $table->foreign('variante_id')->references('id')->on('variantes')->onDelete('cascade');
+            $table->foreign('color_id')->references('id')->on('colores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('variante_color');
+    }
+};
---
+++ database/migrations/2026_03_02_030945_detalle_de_los_productos_vendidos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_ventas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('venta_id')->unsigned();
+            $table->integer('variante_id')->unsigned();
+            $table->integer('cantidad');
+            $table->decimal('precio_cobrado', 12, 2);
+            
+            $table->foreign('venta_id')->references('id')->on('ventas')->onDelete('cascade');
+            $table->foreign('variante_id')->references('id')->on('variantes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_ventas');
+    }
+};
