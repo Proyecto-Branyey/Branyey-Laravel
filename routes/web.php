f77b52460b3c4c4d78fd3f35f66367adb85be573
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-02 19:16:51 -0500

    chore: configuración rutas web.php
    - Rutas configuradas para carpeta tienda (landing page)
    - Rutas configuradas para carpeta backoffice (cruds y todo lo que se encuentra detras con autenticacion)
    - Rutas de Perfil de breeze predeterminadas

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,25 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('tienda')->name('tienda.')->group(function () {
+    
+    Route::get('/', function () {
+        return view('tienda.inicio');
+    })->name('inicio');
+    
+    Route::get('/catalogo', function () {
+        return view('tienda.catalogo');
+    })->name('catalogo');
+    
 });
 
+Route::prefix('backoffice')->name('backoffice.')->middleware(['auth', 'verified'])->group(function () {
+    
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+        return view('backoffice.dashboard');
+    })->name('dashboard');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
