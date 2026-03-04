21d7b6d881966bcfce5d7aeb971533b814d41b54
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-04 16:55:18 -0500

    refactor: entidad Rol
    - añadir metodos utiles para scopes

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -7,11 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class Rol extends Model
 {
     protected $table = 'roles';
-    public $timestamps = false; 
 
-    protected $fillable = ['nombre'];
+    protected $fillable = [
+        'nombre'
+    ];
 
-    public function usuarios() {
+    public function usuarios()
+    {
         return $this->hasMany(User::class, 'rol_id');
+    }
+    
+    /**
+     * Verificar si es un rol específico
+     */
+    public function es(string $nombreRol): bool
+    {
+        return strtolower($this->nombre) === strtolower($nombreRol);
+    }
+
+    public function scopeNombre($query, $nombre)
+    {
+        return $query->where('nombre', $nombre);
+    }
+
+    public function scopeExcluir($query, array $roles)
+    {
+        return $query->whereNotIn('nombre', $roles);
     }
 }
