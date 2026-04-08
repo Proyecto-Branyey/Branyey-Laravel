c7604f8995837057f65d53a70d55e16af5ab8f4d
yeison <davidyeison22@gmail.com>
2026-04-08 12:42:38 -0500

    feat: springboot - se  crean los archivos de springboot

---
+++ Branyey-SpringBoot/src/main/java/com/tuempresa/branyeyspringboot/BranyeySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tuempresa.branyeyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranyeySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranyeySpringBootApplication.class, args);
+	}
+
+}
---
+++ Branyey-SpringBoot/src/test/java/com/tuempresa/branyeyspringboot/BranyeySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuempresa.branyeyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranyeySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
